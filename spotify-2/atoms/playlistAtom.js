89a32bb1ccd27f47ed595519d43b69448d4cab06
steves3142 <steves3142@gmail.com>
2022-10-11 00:44:19 -0400

    Rendering first playlist for center component

--- spotify-2/atoms/playlistAtom.js
+++ spotify-2/atoms/playlistAtom.js
@@ -1,6 +1,12 @@
 import { atom } from "recoil";
 
+export const playlistState = atom({
+
+    key: "playlistState",
+    default: null,
+}); 
+
 export const playlistIdState = atom({
     key: "playlistIdState",
-    default: '2F9fHsFgTEnZOBiK6g5t6c'
-})
+    default: "2F9fHsFgTEnZOBiK6g5t6c",
+}); 
--- spotify-2/components/Center.js
+++ spotify-2/components/Center.js
@@ -1,11 +1,14 @@
 import { useSession } from "next-auth/react"
 import { ChevronDownIcon } from "@heroicons/react/outline";
 import { useEffect, useState } from "react";
-import { shuffle } from "lodash"; 
+import { shuffle } from "lodash";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { playlistIdState, playlistState } from "../atoms/playlistAtom";
+import useSpotify from "../hooks/useSpotify";
 
 const colors = [
     "from-indigo-500",
-    "from-blue-500", 
+    "from-blue-500",
     "from-green-500",
     "from-red-500",
     "from-yellow-500",
@@ -14,20 +17,34 @@ const colors = [
 ];
 
 function Center() {
-    const { data: session } = useSession(); 
+    const { data: session } = useSession();
+    const spotifyApi = useSpotify();
     const [color, setColor] = useState(null);
+    const playlistId = useRecoilValue(playlistIdState)
+    const [playlist, setPlaylist] = useRecoilState(playlistState)
 
     useEffect(() => {
         setColor(shuffle(colors).pop())
-    }, [])
+    }, [playlistId])
+
+    useEffect(() => {
+        spotifyApi
+            .getPlaylist(playlistId)
+            .then((data) => {
+                setPlaylist(data.body);
+            })
+            .catch((err) => console.log("Something went wrong!", err));
+    }, [spotifyApi, playlistId]);
+
+    console.log(playlist); 
 
     return (
         <div className="flex-grow">
             <header className="absolute top-5 right-8">
                 <div className="flex items-center bg-red-300 space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2">
                     <img className="rounded-full w-10 h-10"
-                    src={session?.user.image}
-                    alt=""
+                        src={session?.user.image}
+                        alt=""
                     />
                     <h2>{session?.user.name}</h2>
                     <ChevronDownIcon className="h-5 w-5" />
