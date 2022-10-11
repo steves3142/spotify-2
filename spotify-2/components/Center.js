830663bc1b5e9da66f1a6afcfc0c3cfe85ce4fec
steves3142 <steves3142@gmail.com>
2022-10-11 09:55:53 -0400

    playlist cover image rendered, created Songs component

--- spotify-2/components/Center.js
+++ spotify-2/components/Center.js
@@ -5,6 +5,7 @@ import { shuffle } from "lodash";
 import { useRecoilState, useRecoilValue } from "recoil";
 import { playlistIdState, playlistState } from "../atoms/playlistAtom";
 import useSpotify from "../hooks/useSpotify";
+import Songs from "../components/Songs"; 
 
 const colors = [
     "from-indigo-500",
@@ -50,9 +51,26 @@ function Center() {
                     <ChevronDownIcon className="h-5 w-5" />
                 </div>
             </header>
-            <section className={`flex items-end space-x-7 bg-gradient-to-b to-black ${color} h-80 text-white padding-8`}>
-                <h1>hello</h1>
+
+
+
+            <section className={`flex items-end space-x-7 bg-gradient-to-b to-black ${color} 
+            h-80 text-white padding-8`}
+            >
+                
+                <img className="h-44 w-44 shadow-2xl"
+                src={playlist?.images?.[0]?.url} alt=""/>
+                <div>
+                    <p>PLAYLIST</p>
+                    <h1 className="text-2xl md:text-3xl xl:text-5xl font-bold">
+                        {playlist?.name}
+                    </h1>
+                </div>
             </section>
+
+            <div>
+                <Songs />
+            </div>
         </div>
     );
 }
--- spotify-2/components/Sidebar.js
+++ spotify-2/components/Sidebar.js
@@ -29,7 +29,8 @@ function Sidebar() {
     }, [session, spotifyApi]);
 
     return (
-        <div className="text-gray-500 p-5 text-sm border-r border-gray-900 overflow-y-scroll scrollbar-hide h-screen">
+        <div className="text-gray-500 p-5 text-xs lg:text-sm border-r border-gray-900 overflow-y-scroll 
+        scrollbar-hide h-screen sm:max-w-[12rem] lg:max-w-[15rem]">
             <div className="space-y-4">
             <button className="flex items-center space-x-2
                     hover:text-white"
