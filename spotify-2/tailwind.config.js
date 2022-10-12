419c93720fb72ac6ff70b84d6707508673590d65
steves3142 <steves3142@gmail.com>
2022-10-12 00:45:56 -0400

    adding scrollable songs with album name and duration

--- spotify-2/tailwind.config.js
+++ spotify-2/tailwind.config.js
@@ -4,7 +4,9 @@ module.exports = {
     "./pages/index.js", 
     "./components/Sidebar.js",
     "./pages/login.js",
-    "./components/Center.js"
+    "./components/Center.js", 
+    "./components/Song.js",
+    "./compnents/Songs.js",
   ],
   theme: {
     extend: {},
