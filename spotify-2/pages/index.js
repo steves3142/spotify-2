d87aa1ba813e30e88f39bd17b39522aed7848375
steves3142 <steves3142@gmail.com>
2022-10-12 11:56:48 -0400

    songs are playing upon clickgit

--- spotify-2/pages/index.js
+++ spotify-2/pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Sidebar from '../components/Sidebar'
 import Center from '../components/Center'
+import Player from '../components/Player';
 import { getSession } from 'next-auth/react'
 
 export default function Home() {
@@ -15,8 +16,9 @@ export default function Home() {
         <Center /> 
       </main>
 
-
-      <div>{/* Player */}</div>
+      <div className="sticky bottom-0 ">
+        <Player />
+      </div>
     </div>
   )
 }
@@ -25,7 +27,7 @@ export async function getServerSideProps(context) {
   const session = await getSession(context);
 
   return {
-    props: {
+    props: { 
       session,
     },
   };
