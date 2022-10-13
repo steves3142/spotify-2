05bdbc17770e3ea28116aae8f93ab6a7f766d598
steves3142 <steves3142@gmail.com>
2022-10-12 22:39:46 -0400

    volume working!

--- spotify-2/components/Player.js
+++ spotify-2/components/Player.js
@@ -11,8 +11,9 @@ import {
     VolumeUpIcon,
     SwitchHorizontalIcon,
 } from "@heroicons/react/solid";
+import { debounce } from "lodash"; 
 import { useSession } from "next-auth/react";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useRecoilState } from "recoil";
 import { currentTrackIdState, isPlayingState } from "../atoms/songAtom";
 import useSongInfo from "../hooks/useSonginfo";
@@ -59,6 +60,19 @@ function Player() {
         }
     }, [currentTrackIdState, spotifyApi, session]);
 
+    useEffect(() => {
+        if (volume > 0 && volume < 100) {
+            debounceAdjustVolume(volume); 
+        }
+    }, [volume]);
+
+    const debounceAdjustVolume = useCallback(
+        debounce((volume) => {
+            spotifyApi.setVolume(volume).catch((err) => {});
+        }, 150), 
+        []
+    ); 
+
     return (
         <div className="h-24 bg-gradient-to-b from-black to-gray-900 
         text-white grid grid-cols-3 text-xs md:text-base px-2 md:px-8">
@@ -95,9 +109,10 @@ function Player() {
             {/* Right */}
             <div className="flex items-center space-x-3 
             md:space-x-4 justify-end pr-5">
-                <VolumeDownIcon className="button" />
-                <input className="w-14 md:w-28"  type="range" value={volume} min={0} max={100} /> 
-                <VolumeUpIcon className="button" /> 
+                <VolumeDownIcon onClick={() => volume < 100 && setVolume(volume - 10)}
+                className="button" />
+                <input className="w-14 md:w-28" type="range" value={volume} onChange={e => setVolume(Number(e.target.value))} min={0} max={100} /> 
+                <VolumeUpIcon onClick={() => volume < 100 && setVolume(volume + 10)} className="button" /> 
             </div>
         </div>
     );
